dcb75bf1bd31dbf68dc07640a9d5b6b1757f59ba
meryemsanem <meryemsanemkucukbayram@gmail.com>
2023-06-13 21:04:52 +0300

    Update files

--- src/index.js
+++ src/index.js
@@ -1,13 +1,42 @@
 import './styles/style.css';
 
-function component() {
-  const element = document.createElement('h1');
+const tasks = [
+  {
+    description: 'wash the dishes',
+    completed: false,
+    index: 0,
+  },
+  {
+    description: 'complete To Do List project',
+    completed: false,
+    index: 1,
+  },
+  {
+    description: 'fix car',
+    completed: false,
+    index: 2,
+  },
+  {
+    description: 'go shopping',
+    completed: false,
+    index: 3,
+  },
+];
 
-  // Lodash, now imported by this script
-  element.innerHTML = 'Hello Webpack';
-  element.classList.add('hello');
+const showTasks = () => {
+  const list = document.querySelector('.list');
+  for (let i = 0; i < tasks.length; i += 1) {
+    const listItems = document.createElement('div');
+    listItems.classList.add('to-do--');
+    listItems.innerHTML = `<div class="lists">
+    <input type="checkbox">
+    ${tasks[i].description} 
+  </div><div>
+    <i class="fa-solid fa-ellipsis-vertical"></i>
+    </div>
+  `;
+    list.appendChild(listItems);
+  }
+};
 
-  return element;
-}
-
-document.body.appendChild(component());
+showTasks();
